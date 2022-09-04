770cdb0619517722dabb2fb35a0cb3c8516d5b3c
Ryan Hess <ryanmhess@gmail.com>
2022-09-04 08:43:18 -0500

    adding to heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const { application } = require('express');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 let calcHistory = require('./modules/calculations.js');
 let newCalc;
